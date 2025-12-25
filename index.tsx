56392d10d2294638a77995fdb59953efd3cfaa9b
mohamedmehdisa-wq <mohamedmehdi.sa@gmail.com>
2025-12-25 13:12:25 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -6,6 +5,19 @@ import App from './App';
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
+}
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    // Le SW sera à la racine après le build
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('VibrerStade SW registered');
+      })
+      .catch(err => {
+        console.log('SW registration failed: ', err);
+      });
+  });
 }
 
 const root = ReactDOM.createRoot(rootElement);
