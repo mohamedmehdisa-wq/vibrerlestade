dde38e2966913a1e6f0eb99e6a874245e63f5c45
mohamedmehdisa-wq <mohamedmehdi.sa@gmail.com>
2025-12-25 12:06:27 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
-
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const apiKey = process.env.API_KEY || env.API_KEY;
+  const apiKey = process.env.API_KEY || env.API_KEY || '';
 
   return {
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(apiKey)
-    },
-    server: {
-      port: 3000
     },
     build: {
       outDir: 'dist',
